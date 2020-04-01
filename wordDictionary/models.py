1616cc61ed47543b0d5b461d8f5970da1ebe5b9e
Lazaron <lazaronshyta@gmail.com>
2020-04-01 13:27:47 +0300

    final

--- wordDictionary/models.py
+++ wordDictionary/models.py
@@ -1,5 +1,4 @@
 from djongo import models
-<<<<<<< HEAD
 from django import forms
 from jsonfield import JSONField
 from django.contrib.auth.models import User
@@ -161,40 +160,3 @@ class Word(models.Model):
     )
     def __str__(self):
         return self.name
-=======
-
-from jsonfield import JSONField
-# Create your models here.
-
-
-class Feature(models.Model):
-    featureName = models.CharField(max_length=20)
-    currDimension = models.CharField(max_length=20, default=None)
-    def __str__(self):
-        return self.featureName
-
-class Language(models.Model):
-    name = models.CharField(max_length=15)
-    def __str__(self):
-        return self.name
-
-class Dimension(models.Model):
-    dimensionName = models.CharField(max_length=20)
-    features = models.ArrayReferenceField(
-        to = Feature,
-        default=None,
-    )
-    def __str__(self):
-        return self.dimensionName
-
-class Word(models.Model):
-    wordContent = models.CharField(max_length=50)
-    rootWord = models.CharField(max_length=50)
-    language = models.ArrayReferenceField(
-        to = Language,
-        default=None,
-    )
-    dimensions = JSONField()
-    def __str__(self):
-        return self.wordContent
->>>>>>> upstream/master
