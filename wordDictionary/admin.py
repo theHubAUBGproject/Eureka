bbc95024f8758cbfdf709819a6fc793295f18092
Lazaron <lazaronshyta@gmail.com>
2020-04-10 19:22:15 +0300

    Add more models

--- wordDictionary/admin.py
+++ wordDictionary/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from django.contrib.admin.models import LogEntry
-from wordDictionary.models import Word, Feature, Dimension, Language
+from wordDictionary.models import Genus, Word, Feature, Dimension, Language, Lemma, Family, TagSet, POS
+
 
 
 # Register your models here.
@@ -8,4 +8,9 @@ admin.site.register(Word)
 admin.site.register(Feature)
 admin.site.register(Dimension)
 admin.site.register(Language)
-admin.site.register(LogEntry)
+admin.site.register(Lemma)
+admin.site.register(Family)
+admin.site.register(TagSet)
+admin.site.register(POS)
+admin.site.register(Genus)
+#admin.site.register(LogEntry)
--- wordDictionary/models.py
+++ wordDictionary/models.py
@@ -1,36 +1,184 @@
 from djongo import models
-
+from django import forms
 from jsonfield import JSONField
+from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
 
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name','email']
 
-class Feature(models.Model):
-    featureName = models.CharField(max_length=20)
-    currDimension = models.CharField(max_length=20, default=None)
-    def __str__(self):
-        return self.featureName
-
-class Language(models.Model):
+#++++++++++++++++++++++++++
+class Genus(models.Model):
     name = models.CharField(max_length=15)
     def __str__(self):
         return self.name
 
+class GenusForm(forms.ModelForm):
+    class Meta:
+        model=Genus
+        fields=['name']
+
+#****************************
+class Family(models.Model):
+    name = models.CharField(max_length=15)
+    def __str__(self):
+        return self.name
+
+class FamilyForm(forms.ModelForm):
+    class Meta:
+        model = Family
+        fields = ['name']
+#++++++++++++++++++++++++++
+
+class Language(models.Model):
+    name = models.CharField(max_length=15)
+    
+    family = models.EmbeddedField(
+        model_container = Family,
+        model_form_class = FamilyForm,
+        default = None,
+    )
+
+    genus = models.EmbeddedField(
+        model_container=Genus,
+        model_form_class=GenusForm,
+        default=None,
+    )
+
+    walsCode = models.CharField(max_length=15,blank=True)
+    def __str__(self):
+        return self.name
+
+class LanguageForm(forms.ModelForm):
+    class Meta:
+        model = Language
+        fields = ['name']
+
+#++++++++++++++++++++++++++++
+
 class Dimension(models.Model):
-    dimensionName = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
+
+class DimensionForm(forms.ModelForm):
+    class Meta:
+        model = Dimension
+        fields = ['name']
+
+#++++++++++++++++++++++++
+
+class Feature(models.Model):
+    name = models.CharField(max_length=20)
+    dimension = models.EmbeddedField(
+        model_container = Dimension,
+        model_form_class = DimensionForm,
+        default=None,
+    )
+
+    def __str__(self):
+        return self.name
+
+class FeatureForm(forms.ModelForm):
+    class Meta:
+        model = Feature
+        fields = ['name']
+
+#++++++++++++++++++++++++
+
+class POS(models.Model):
+    name = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
+
+class POSForm(forms.ModelForm):
+    class Meta:
+        model = POS
+        fields = ['name']
+
+
+
+#++++++++++++++++++++++++
+
+class Lemma(models.Model):
+    name = models.CharField(max_length=50)
+    language = models.EmbeddedField(
+        model_container = Language,
+        model_form_class = LanguageForm,
+        blank=True,
+        default = None,
+    )
+
+    animacy = models.BooleanField(default=True)
+    transivity = models.BooleanField(default=True)
+
+    
+    author = models.EmbeddedField(
+        model_container = User,
+        model_form_class = UserForm,
+        blank = True,
+        default = None,
+    )
+    pos = models.EmbeddedField(
+        model_container = POS,
+        model_form_class = POSForm,
+        blank=True,
+        default = None,
+    )
+    date_updated = models.DateTimeField(default=timezone.now)
+    def __str__(self):
+        return self.name
+
+class LemmaForm(forms.ModelForm):
+    class Meta:
+        model = Lemma
+        fields = ['name']
+
+#++++++++++++++++++++++++
+
+
+class TagSet(models.Model):
+    name = models.CharField(max_length=50,blank=True, default="")
     features = models.ArrayReferenceField(
         to = Feature,
-        default=None,
+        default=None
     )
     def __str__(self):
-        return self.dimensionName
+        return self.name
 
+class TagSetForm(forms.ModelForm):
+    class Meta:
+        model = TagSet
+        fields = ['name','features']
+
+#++++++++++++++++++++++++
 class Word(models.Model):
-    wordContent = models.CharField(max_length=50)
-    rootWord = models.CharField(max_length=50)
-    language = models.ArrayReferenceField(
-        to = Language,
+    name = models.CharField(max_length=50)
+    date_updated = models.DateTimeField(default=timezone.now)
+    author = models.EmbeddedField(
+        model_container = User,
+        model_form_class = UserForm,
+        blank = True,
+        default = None,
+    )
+    lemma = models.EmbeddedField(
+        model_container = Lemma,
+        model_form_class = LemmaForm,
         default=None,
     )
-    dimensions = JSONField()
+
+    language = models.EmbeddedField(
+        model_container = Language,
+        model_form_class = LanguageForm,
+        default=None,
+    )
+    tagset = models.EmbeddedField(
+        model_container  = TagSet,
+        model_form_class = TagSetForm,
+        default=None,
+    )
     def __str__(self):
-        return self.wordContent
+        return self.name
